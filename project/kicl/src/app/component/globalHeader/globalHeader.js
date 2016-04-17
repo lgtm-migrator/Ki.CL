98dec86bf181841e823902aed66feb70cb8954db
Keni Lam <hello@ki-cl.com>
2016-04-17 14:05:06 -0700

    revert changes

--- project/kicl/src/app/component/globalHeader/globalHeader.js
+++ project/kicl/src/app/component/globalHeader/globalHeader.js
@@ -66,13 +66,7 @@
 				};
 
 				this.close = function (event) {
-					if (event.target &&
-						(
-							event.target.parentNode.classList.contains('navigation') ||
-							event.target.parentNode.parentNode.classList.contains('navigation') ||
-							event.target.parentNode.parentNode.parentNode.classList.contains('navigation')
-						)
-					) {
+					if (event.target && event.target.parentElement.classList.contains('navigation')) {
 						return;
 					}
 
@@ -199,5 +193,4 @@
 			]
 		);
 }());
-
 
