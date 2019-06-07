69fa3a29e2c40d90cb379aa96691b6513074f6bf
kenilam <hello@ki-cl.com>
2019-06-07 01:44:08 -0700

    [FEAT] Bump several modules
     * Migrated autoprefix from postcssrc to browserslistrc
     * Migrated deprecated namedModules to optimization options
     * Fixed CleanWebpackPlugin new import syntex
     * Added trianglify

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,12 +6,6 @@ const autoprefixer = require('autoprefixer')
 module.exports = {
   plugins: [
     autoprefixer({
-      browsers: [
-        '>1%',
-        'last 4 versions',
-        'Firefox ESR',
-        'not ie < 9', // React doesn't support IE8 anyway
-      ],
       cascade: false,
       flexbox: 'no-2009',
       grid: 'autoplace'
